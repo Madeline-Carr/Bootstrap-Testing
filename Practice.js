fb76a921034b2aacf9530be522c0ba4626a01b7c
Madeline Carr <Carrm2@vcu.edu>
2018-06-20 21:41:41 -0400

    NewSass
    I made new Sass files.

--- Practice.js
+++
@@ -1,8 +0,0 @@
-
-function myFunction() {
-    var person = prompt("What is your name?", "Tony Stark");
-    if (person != null) {
-        document.getElementById("demo").innerHTML =
-        "Hello " + person + "! How are you today?";
-    }
-}
